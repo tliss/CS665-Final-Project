0b7bf02053f15836e7a530bc545ecea9bebe940a
Taylor A. Liss <tayloraliss@gmail.com>
2021-08-11 00:36:26 -0400

    Refactoring

--- src/main/java/edu/bu/met/cs665/Arena.java
+++ src/main/java/edu/bu/met/cs665/Arena.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 import org.apache.log4j.Logger;
 
 /**
- * This class is used to create an arena object in which two spaceships can battle. Ships take
- * turns attack eachother until one of them is destroyed.
+ * This class is used to create an arena object in which two spaceships can battle. Ships take turns
+ * attack eachother until one of them is destroyed.
  */
 public class Arena {
+
   private static final Logger logger = Logger.getLogger(Arena.class);
 
   private SpaceShip shipA;
@@ -25,50 +26,59 @@ public class Arena {
     logger.info("Fighter 1: " + shipA.getName() + " " + Arrays.toString(shipA.getLoadout()));
     logger.info("Fighter 2: " + shipB.getName() + " " + Arrays.toString(shipB.getLoadout()));
 
+    SpaceShip fastestShip = null;
+    SpaceShip slowestShip = null;
+    if (shipA.getSpeed() > shipB.getSpeed()) {
+      fastestShip = shipA;
+      slowestShip = shipB;
+    } else {
+      fastestShip = shipB;
+      slowestShip = shipA;
+    }
+    logger.info(fastestShip.getName() + " has the speed advantage!");
+
     boolean result = false;
     while (!result) {
-      result = exchangeFire();
+      result = exchangeFire(fastestShip, slowestShip);
     }
   }
 
   /**
-   * Ships take turns shooting at eachother. The ship with the higher thruster speed shoots
-   * first. If the ships have the same thruster speed, then shipA shoots first.
-   * @return true if one of the ships has been destroyed.
+   * Fixes names by adding a space in between lowercase and capital letters.
+   * @param name the name to fix
+   * @return the fixed name
    */
-  private boolean exchangeFire() {
-    if (shipA.getSpeed() > shipB.getSpeed()) {
-      logger.info(shipA.getName() + " has the speed advantage!");
-      shipA.fireAt(shipB);
-      logger.info(shipA.getName() + "'s " + shipA.getLoadout()[1] + " damages " + shipB.getName()
-        + "'s " + shipB.getLoadout()[2] + " for " + shipA.getStrength() + " damage!");
-      if (shipB.isDestroyed()) {
-        logger.info(shipB.getName() + " has been destroyed! " + shipA.getName() + " is the winner!");
-        return true;
-      }
-      shipB.fireAt(shipA);
-      logger.info(shipB.getName() + "'s " + shipB.getLoadout()[1] + " damages " + shipA.getName()
-          + "'s " + shipA.getLoadout()[2] + " for " + shipB.getStrength() + " damage!");
-      if (shipA.isDestroyed()) {
-        logger.info(shipA.getName() + " has been destroyed! " + shipB + " is the winner!");
-        return true;
-      }
-    } else {
-      logger.info(shipB.getName() + " has the speed advantage!");
-      shipB.fireAt(shipA);
-      logger.info(shipB.getName() + "'s " + shipB.getLoadout()[1] + " damages " + shipA.getName()
-          + "'s " + shipA.getLoadout()[2] + " for " + shipB.getStrength() + " damage!");
-      if (shipA.isDestroyed()) {
-        logger.info(shipA.getName() + " has been destroyed! " + shipB.getName() + " is the winner!");
-        return true;
-      }
-      shipA.fireAt(shipB);
-      logger.info(shipA.getName() + "'s " + shipA.getLoadout()[1] + " damages " + shipB.getName()
-          + "'s " + shipB.getLoadout()[2] + " for " + shipA.getStrength() + " damage!");
-      if (shipB.isDestroyed()) {
-        logger.info(shipB.getName() + " has been destroyed! " + shipA + " is the winner!");
-        return true;
-      }
+  private String fixName(String name) {
+    return name.replaceAll("\\d+", "")
+        .replaceAll("(.)([A-Z])", "$1 $2");
+  }
+
+  /**
+   * Ships take turns shooting at eachother. The ship with the higher thruster speed shoots first.
+   * If the ships have the same thruster speed, then shipA shoots first.
+   *
+   * @return true once one of teh ships has been destroyed.
+   */
+  private boolean exchangeFire(SpaceShip fastShip, SpaceShip slowShip) {
+    String fastShipWeapon = fixName(fastShip.getLoadout()[1]);
+    String slowShipWeapon = fixName(slowShip.getLoadout()[1]);
+    String fastShipHull = fixName(fastShip.getLoadout()[2]);
+    String slowShipHull = fixName(slowShip.getLoadout()[2]);
+
+    fastShip.fireAt(slowShip);
+    logger.info(fastShip.getName() + "'s " + fastShipWeapon + " damages " + slowShip.getName()
+        + "'s " + slowShipHull + " for " + fastShip.getStrength() + " damage!");
+    if (slowShip.isDestroyed()) {
+      logger.info(
+          shipB.getName() + " has been destroyed! " + fastShip.getName() + " is the winner!");
+      return true;
+    }
+    slowShip.fireAt(fastShip);
+    logger.info(slowShip.getName() + "'s " + slowShipWeapon + " damages " + fastShip.getName()
+        + "'s " + fastShipHull + " for " + slowShip.getStrength() + " damage!");
+    if (fastShip.isDestroyed()) {
+      logger.info(fastShip.getName() + " has been destroyed! " + slowShip.getName() + " is the winner!");
+      return true;
     }
     return false;
   }
